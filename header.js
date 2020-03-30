b56f23ec4410833b36b1f1e4cc884ec943583bfa
Aylor Brown <aylorbrown@gmail.com>
2020-03-30 17:06:59 -0400

    adds box shadow to header, on scroll

--- header.js
+++ header.js
@@ -6,17 +6,28 @@ const headerTag = document.querySelector("header")
 const toggleHeader = function () {
     const pixels = window.pageYOffset
 
-    if (pixels > 60) {
+    if (pixels > 64) {
         headerTag.classList.add("scrolled")
     } else {
         headerTag.classList.remove("scrolled")
     }
 }
 
+const fadeBox = function () {
+    // pixels is how far youve scrolled 
+    const pixels = window.pageYOffset
+    //weighted it by dividing by 1000 so it fades in a lot slower 
+    const alpha = Math.min(pixels / 1000, 0.5)
+
+    headerTag.style.boxShadow = `0 0 10px rgba(0, 0, 0, ${alpha})`
+}
+
 //on load 
+fadeBox()
 toggleHeader()
 
 // on scroll 
 document.addEventListener("scroll", function () {
     toggleHeader()
+    fadeBox()
 })
